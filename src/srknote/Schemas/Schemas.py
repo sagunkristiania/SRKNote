796058ba97edc8ecaaf9f8aa956ac5581808e4db
sagunkristiania <sakh024@student.kristiania.no>
2025-10-14 20:10:22 +0545

    added few validation

--- src/srknote/Schemas/Schemas.py
+++ src/srknote/Schemas/Schemas.py
@@ -1,8 +1,8 @@
-
 from typing import Optional
 from datetime import datetime
-from pydantic import BaseModel, EmailStr, field_validator, SecretStr
+from pydantic import BaseModel, EmailStr, field_validator, SecretStr, validator, Field
 from uuid import UUID
+
 
 class RegisterIn(BaseModel):
     name: str
@@ -15,44 +15,56 @@ class RegisterIn(BaseModel):
             raise ValueError("Password must be at least 8 characters, include a number and an uppercase letter")
         return v
 
+
 class LoginIn(BaseModel):
     email: EmailStr
     password: str
 
+
 class TokenOut(BaseModel):
     access_token: str
     token_type: str = "bearer"
+
 
 class MeOut(BaseModel):
     id: UUID
     name: str
     email: EmailStr
 
+
 class UserSchema(BaseModel):
-    id:int
-    name:str
-    email:EmailStr
-    password_hash:str
+    id: int
+    name: str
+    email: EmailStr
+    password_hash: str
 
 
 class NoteIn(BaseModel):
     title: str
     content: str
-    user_enc:bool
+    user_enc: bool
+
 
 class NoteSchema(BaseModel):
-    id: Optional[int]=None
+    id: Optional[int] = None
     user_id: UUID
     title: str
     content: str
-    user_enc:bool
-    enc_key:str
+    user_enc: bool
+    enc_key: str
+
 
 class CreateNoteSchema(BaseModel):
     title: str
     content: str
-    user_enc:bool
-    enc_key:Optional[str] = None
+    user_enc: bool = False
+    enc_key: Optional[str] = None
+
+    @field_validator('enc_key')
+    def check_enc_key(self, v, values):
+        if values.get('user_enc') and not v:
+            raise ValueError('Please provide Your Encryption Password')
+        return v
 
 
 class NoteUpdate(BaseModel):
@@ -60,10 +72,11 @@ class NoteUpdate(BaseModel):
     content: Optional[str] = None
     enc_key: Optional[str] = None
 
+
 class UserRegister(BaseModel):
-    name: str
+    name: str = Field(..., max_length=50)
     email: EmailStr
-    password: SecretStr
+    password: SecretStr = Field(..., min_length=8)
 
 
 class UserLogin(BaseModel):
@@ -86,11 +99,9 @@ class DecryptRequest(BaseModel):
     encrypted_data: str
 
 
-
 class NoteCreate(BaseModel):
-    title: str
-    content: str
-
+    title: str = Field(..., max_length=50, )
+    content: str = Field(..., max_length=5000)
 
 
 class NoteResponse(BaseModel):
